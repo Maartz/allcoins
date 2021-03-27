b957d524ce17ca795bcef918d8be12a53cd54a9e
William MARTIN <william.martin@hey.com>
2021-03-27 07:38:28 +0100

    Add a Historical GenServer module to keep state of Trade ... I did it for the lulz! probably gonna be bottlenecked and I'll regret it

---
+++ lib/allcoins/historical.ex
@@ -0,0 +1,44 @@
+defmodule Allcoins.Historical do
+  use GenServer
+  alias Allcoins.{Product, Trade, Exchanges}
+
+  @type historical :: %__MODULE__{
+    products: [Product.t()],
+    trades: %{
+      Product.t() => Trade.t()
+    }
+  }
+  defstruct [:products, :trades]
+
+  @spec get_last_trade(pid() | atom(), Product.t()) :: Trade.t() | nil
+  def get_last_trade(pid \\__MODULE__, product) do
+    GenServer.call(pid, {:get_last_trade, product})
+  end
+
+  # :products
+  def start_link(opts) do
+    {products, opts} = Keyword.pop(opts, :products, []) # past an empty as default value to avoid genserver to crash
+    GenServer.start_link(__MODULE__, products, opts)
+  end
+
+  def init(products) do
+    historical = %__MODULE__{products: products, trades: %{}}
+    {:ok, historical, {:continue, :subscribe}}
+  end
+
+  def handle_continue(:subscribe, historical) do
+    Enum.each(historical.products, &Exchanges.subscribe/1)
+    {:noreply, historical}
+  end
+
+  def handle_info({:new_trade, trade}, historical) do
+    updatead_trades = Map.put(historical.trades, trade.product, trade)
+    updatead_historical = %{historical | trades: updatead_trades}
+    {:noreply, updatead_historical}
+  end
+
+  def handle_call({:get_last_trade, product}, _from, historical) do
+    trade = Map.get(historical.trades, product)
+    {:reply, trade, historical}
+  end
+end
